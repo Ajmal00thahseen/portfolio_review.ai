431636b51ae1907a753652a49b16ef065b15fd1a
Ajmal <ajmalasraf000@gmail.com>
2025-07-25 19:26:50 +0530

    Add onboarding UI components: Hero, Button, FeatureBox, InsightSection, Header

---
+++ src/app/components/InsightSection.tsx
@@ -0,0 +1,18 @@
+const InsightSection = () => {
+  return (
+    <div className="flex flex-col ml-75 w-4xl m-16">
+      <p className="text-4xl font-extrabold mb-3">How it works</p>
+      <p>
+        Our AI goes beyond basic analysis, offering a comprehensive review
+        tailored to industry standards and best practices. Get actionable
+        insights to enhance your portfolio's performance and stand out.
+        <br />
+        <br />
+        Portfolio.ai uses advanced AI to provide a deep dive into your
+        portfolio. We analyze your assets against current industry benchmarks
+        and proven best practices, giving you a clear roadmap for improvement.
+      </p>
+    </div>
+  );
+};
+export default InsightSection;
